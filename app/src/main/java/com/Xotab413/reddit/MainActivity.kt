b004393a978bec940791fcc8528a155ca212ec4e
Xotab413 <sem.matusevich@mail.ru>
2021-09-24 11:04:38 +0300

    update project

---
+++ app/src/main/java/com/Xotab413/reddit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.Xotab413.reddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kts.reddit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
