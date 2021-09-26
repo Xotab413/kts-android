4fdf1ab8ef29fee7da7ae78098199d6ee4c42609
Xotab413 <sem.matusevich@mail.ru>
2021-09-26 22:53:06 +0300

    Init commit

--- app/src/main/java/com/xotab413/reddit/MainActivity.kt
+++ app/src/main/java/com/xotab413/reddit/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.Xotab413.reddit
+package com.xotab413.reddit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kts.reddit.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        main_button.setOnClickListener {
+//            Toast.makeText(this, "Hello, i'm funny toast",Toast.LENGTH_LONG).show()
+//        }
     }
 }
