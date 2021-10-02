d9049d269a32f9bba4cc7cce53f2acbef902213d
Xotab413 <sem.matusevich@mail.ru>
2021-10-02 21:37:38 +0300

    fix som-thinh

--- app/src/main/java/com/xotab413/reddit/LoginFragment.kt
+++ app/src/main/java/com/xotab413/reddit/LoginFragment.kt
@@ -1,16 +1,13 @@
 package com.xotab413.reddit
 
-import androidx.fragment.app.Fragment
-import com.kts.reddit.R
 import android.os.Bundle
-import android.util.Patterns
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.kts.reddit.R
 import kotlinx.android.synthetic.main.fragment_login.*
-import kotlinx.android.synthetic.main.fragment_onboarding.*
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
-
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -23,7 +20,4 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                         findNavController().navigate(R.id.mainFragment)
                     }
             }
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 }
