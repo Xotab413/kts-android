dd916e05fb1ff0c79f4d7cd0931fc87befb6e744
Xotab413 <sem.matusevich@mail.ru>
2021-10-04 02:05:32 +0300

    add navigation&valid input

--- app/src/main/java/com/xotab413/reddit/LoginFragment.kt
+++ app/src/main/java/com/xotab413/reddit/LoginFragment.kt
@@ -1,23 +1,59 @@
 package com.xotab413.reddit
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kts.reddit.R
+import com.kts.reddit.databinding.FragmentLoginBinding
 import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var binding: FragmentLoginBinding
+
+    fun validInput(): Boolean {
+
+        val isValidEmail = Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.text).matches()
+        return (isValidEmail && binding.etPassword.text.length >= 8)
+    }
+
+    private fun makeWatchers(binding: FragmentLoginBinding): TextWatcher {
+        val textWatcher: TextWatcher = object : TextWatcher {
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.btnLogin.isEnabled = validInput()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+        }
+        return textWatcher
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("text", etEmail.text.toString())
-        outState.putString("text", etPassword.text.toString())
+        outState.putString("email", binding.etEmail.text.toString())
+        outState.putString("password", binding.etPassword.text.toString())
     }
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-                    btnLogin.setOnClickListener {
-                        findNavController().navigate(R.id.mainFragment)
-                    }
-            }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.etPassword.addTextChangedListener(makeWatchers(binding))
+        binding.etEmail.addTextChangedListener(makeWatchers(binding))
+
+        binding.btnLogin.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/xotab413/reddit/OnboardingFragment.kt
+++ app/src/main/java/com/xotab413/reddit/OnboardingFragment.kt
@@ -1,17 +1,20 @@
 package com.xotab413.reddit
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kts.reddit.R
 import kotlinx.android.synthetic.main.fragment_onboarding.*
 
+
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         btnEnjoy.setOnClickListener {
-            findNavController().navigate(R.id.loginFragment)
+            val action = OnboardingFragmentDirections.actionOnboardingFragmentToLoginFragment()
+            findNavController().navigate(action)
         }
     }
 }
